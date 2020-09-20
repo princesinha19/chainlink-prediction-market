8834c08c41bb56a49b58ec92302efc88b0b46432
ayushkaul <ayushk.kaul3@gmail.com>
2020-09-21 02:44:48 +0530

    generate code for subgraph

--- subgraph/chainlink-prediction-market/src/mapping.ts
+++ subgraph/chainlink-prediction-market/src/mapping.ts
@@ -1,22 +1,24 @@
 // import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
 // import { Gravatar } from '../generated/schema'
+import {NewPrediction, AaveLend, MarketResolved, Withdrawn} from '../generated/chainlink prediction market/PredictionMarket'
+import {Prediction, Lend, Market, Position, Predictor} from '../generated/schema'
 
 export function handleNewPrediction(event: NewPrediction): void {
-  let prediction = new Prediction(event.transaction.hash.toHex() +
-    "-" + event.logIndex.toString())
+  let prediction = new Prediction(event.params.predictor.toString() +
+    '-' + event.params.prediction.toString()+ '-' +
+    event.params.stake.toString())
   prediction.predictor = event.params.predictor
   prediction.prediction = event.params.prediction
   prediction.stake = event.params.stake
   prediction.save()
 
-  let predictor = new Predictor(event.params.predictor)
+  let predictor = new Predictor(event.params.predictor.toString())
   predictor.address = event.params.predictor
   predictor.activeStake = event.params.stake
 }
 
 export function handleAaveLend(event: AaveLend): void {
-  let aaveLend = new AaveLend(event.transaction.hash.toHex() +
-    "-" + event.logIndex.toString())
+  let aaveLend = new Lend(event.params.amount.toString())
   aaveLend.amount = event.params.amount
 }
 
@@ -27,8 +29,8 @@ export function handleMarketResolve(event: MarketResolved): void {
 
 // update the predictor stake
 export function handleWithdraw(event: Withdrawn): void {
-  let id = event.params.address
+  let id = event.params.withdrawer.toString()
   let predictor = Predictor.load(id)
   let oldStake = predictor.activeStake
-  predictor.activeStake = oldStake + event.params.activeStake
+  predictor.activeStake = oldStake.minus(event.params.amount)
 }
