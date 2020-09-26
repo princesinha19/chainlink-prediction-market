be1ff78847a5ff47b9ae435a3ccfdd386fee430d
Prince Sinha <sinhaprince013@gmail.com>
2020-09-26 17:55:13 +0530

    Updates

---
+++ precache-manifest.8171f4a576605e4415a12219d53745a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fca79aa7b1b0381d37c398c1735e5e",
+    "url": "/chainlink-prediction-market/index.html"
+  },
+  {
+    "revision": "0e26e20c520a421e3768",
+    "url": "/chainlink-prediction-market/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "36b507ce4e383c55c5f5",
+    "url": "/chainlink-prediction-market/static/css/main.a60c3d85.chunk.css"
+  },
+  {
+    "revision": "0e26e20c520a421e3768",
+    "url": "/chainlink-prediction-market/static/js/2.05805d91.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/chainlink-prediction-market/static/js/2.05805d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b507ce4e383c55c5f5",
+    "url": "/chainlink-prediction-market/static/js/main.0e0e940c.chunk.js"
+  },
+  {
+    "revision": "dd55795f9ad40b6fecbf",
+    "url": "/chainlink-prediction-market/static/js/runtime-main.7151e874.js"
+  }
+]);
